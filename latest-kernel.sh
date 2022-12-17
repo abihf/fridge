46201ebe9a1d954b4ffa91d3b72381b96fa74327
Abi Hafshin <abi@hafs.in>
2022-12-17 19:39:04 +0700

    update

--- latest-kernel.sh
+++ latest-kernel.sh
@@ -1,3 +1,3 @@
 hash=$(curl -Ls "https://raw.githubusercontent.com/raspberrypi/firmware/master/extra/git_hash")
 
-sed -i "/^BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=/c\\BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"\$(call github,raspberrypi,linux,$hash)/linux-$hash.tar.gz\"" 4/.config
+sed -i "/^BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=/c\\BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"\$(call github,raspberrypi,linux,$hash)/linux-$hash.tar.gz\"" 4/.config external/configs/rpi4_fridge_defconfig
